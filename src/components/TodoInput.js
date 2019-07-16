7460dc40776686dbc25eb4a8d3eea883b1c92cc7
unknown <jeff_ansay@ligph.com>
2019-07-16 10:08:17 +0800

    initial commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Todo Input</h2>
+            </div>
+        );
+    }
+}
+
+export default TodoInput;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoItem = () => {
+    return (
+        <div>
+            <h2>Hello from Item</h2>
+        </div>
+    );
+}
+
+export default TodoItem;
